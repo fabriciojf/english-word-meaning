b59f5a0845297e3b01fdde89d469f4578c864298
fabricio <fabriciojf@gmail.com>
2020-10-21 20:00:56 -0300

    Update informations in basic classes

--- src/main/java/com/fabriciojf/english/EnglishApplication.java
+++ src/main/java/com/fabriciojf/english/EnglishApplication.java
@@ -3,10 +3,18 @@ package com.fabriciojf.english;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Start point of Spring Boot
+ *
+ * @author Fabricio S Costa fabriciojf@gmail.com
+ * @class WordController
+ * @version 1.0
+ * @since 21/10/2020
+ */
 @SpringBootApplication
 public class EnglishApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EnglishApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EnglishApplication.class, args);
+    }
 }
--- src/main/java/com/fabriciojf/english/config/DataConfiguration.java
+++ src/main/java/com/fabriciojf/english/config/DataConfiguration.java
@@ -10,12 +10,18 @@ import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
+/**
+ * @author Fabricio S Costa fabriciojf@gmail.com
+ * @class DataConfiguration
+ * @version 1.0
+ * @since 21/10/2020
+ */
 @Configuration
 @Profile("dev")
 public class DataConfiguration {
 
-	@Bean
-    public DataSource dataSource(){
+    @Bean
+    public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("com.mysql.jdbc.Driver");
         dataSource.setUrl("jdbc:mysql://localhost:3306/englishwords");
@@ -23,15 +29,15 @@ public class DataConfiguration {
         dataSource.setPassword("cp65482jf");
         return dataSource;
     }
-	
-	@Bean
-	public JpaVendorAdapter jpaVendorAdapter(){
-		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
-		adapter.setDatabase(Database.MYSQL);
-		adapter.setShowSql(true);
-		adapter.setGenerateDdl(true);
-		adapter.setDatabasePlatform("org.hibernate.dialect.MariaDBDialect");		
-		adapter.setPrepareConnection(true);
-		return adapter;
-	}
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter() {
+        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+        adapter.setDatabase(Database.MYSQL);
+        adapter.setShowSql(true);
+        adapter.setGenerateDdl(true);
+        adapter.setDatabasePlatform("org.hibernate.dialect.MariaDBDialect");
+        adapter.setPrepareConnection(true);
+        return adapter;
+    }
 }
